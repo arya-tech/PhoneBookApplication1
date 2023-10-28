04405fe06196244a90d8b4d92f8b569929e633ba
arya-tech <aryadewan123@gmail.com>
2023-10-28 20:02:53 +0530

    new field added.

--- PhoneBookApplication/src/main/java/com/phonebook/constant/ApplicationConstant.java
+++ PhoneBookApplication/src/main/java/com/phonebook/constant/ApplicationConstant.java
@@ -8,4 +8,7 @@ public class ApplicationConstant {
 	// error message added
 	public static final String ERROR_MESSAGE="error occured while saving";
 	public static final String EXCEPTION_MSG="exception occured while working";
+	// for new exception
+	public static final String EXCEPTION_MSG1="exception occured while working";
+	
 }
