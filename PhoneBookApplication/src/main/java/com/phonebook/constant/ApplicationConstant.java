1db5cd19947212a98033b604cffbdeb593aeb78a
arya-tech <aryadewan123@gmail.com>
2023-10-28 17:21:09 +0530

    ERROR_MESSAGE filed added in ApplicationConstant class.

--- PhoneBookApplication/src/main/java/com/phonebook/constant/ApplicationConstant.java
+++ PhoneBookApplication/src/main/java/com/phonebook/constant/ApplicationConstant.java
@@ -5,4 +5,5 @@ public class ApplicationConstant {
 	public static final String SUCCESS_MSG="saveSuccess";
 	public static final String UPDATE_MSG="updateSuccess";
 	public static final String FAILED_TO_SAVE="failedSave";
+	public static final String ERROR_MESSAGE="error occured while saving";
 }
