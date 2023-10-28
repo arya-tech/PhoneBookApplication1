937efaef619d2e304c04300c7beb01e2e36f95c2
arya-tech <aryadewan123@gmail.com>
2023-10-28 17:26:21 +0530

    comment added

--- PhoneBookApplication/src/main/java/com/phonebook/constant/ApplicationConstant.java
+++ PhoneBookApplication/src/main/java/com/phonebook/constant/ApplicationConstant.java
@@ -5,5 +5,6 @@ public class ApplicationConstant {
 	public static final String SUCCESS_MSG="saveSuccess";
 	public static final String UPDATE_MSG="updateSuccess";
 	public static final String FAILED_TO_SAVE="failedSave";
+	// error message added
 	public static final String ERROR_MESSAGE="error occured while saving";
 }
